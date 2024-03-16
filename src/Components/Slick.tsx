e199724f85c67f1df256366c6b1d1f6c025b1756
Arata <aratadayoon@yahoo.co.jp>
2024-03-16 17:39:26 +0900

    slick

---
+++ src/Components/Slick.tsx
@@ -0,0 +1,11 @@
+export const settings = {
+    dots: true,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4500,
+    arrows: false,
+    pauseOnHover: false,
+  };
--- src/Portfolio/TopPage.tsx
+++ src/Portfolio/TopPage.tsx
@@ -3,10 +3,13 @@ import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
 
 import { frontend, backend, tools } from '../Components/Skills'
-import { posts } from '../Components/Favorite'
 import { footerNavigation } from '../Components/FooterNav'
+import { settings } from '../Components/Slick'
 
 function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(' ');
@@ -198,34 +201,107 @@ export default function Example() {
       {/* アピールエリア */}
 
       <div className="py-20 sm:py-20 bg-gray-800">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl text-center">
-          <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">My Favorite</h2>
-        </div>
-        <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {posts.map((post) => (
-            <article key={post.id} className="flex flex-col items-start justify-between">
-              <div className="relative w-full">
-                <img
-                  src={post.imageUrl}
-                  alt=""
-                  className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
-                />
-                <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
-              </div>
-              <div className="max-w-xl">
-                <div className="group relative">
-                  <h3 className="mt-3 text-lg font-semibold leading-6 text-white group-hover:text-gray-600">
-                    <a href={post.href}>
-                      <span className="absolute inset-0" />
-                      {post.title}
-                    </a>
-                  </h3>
-                  <p className="mt-5 line-clamp-3 text-sm leading-6 text-white">{post.description}</p>
-                </div>
-              </div>
-            </article>
-          ))}
+  <div className="mx-auto max-w-7xl px-6 lg:px-8">
+    <div className="mx-auto max-w-2xl text-center">
+      <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">My Favorite</h2>
+    </div>
+    <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+
+<div>
+          <Slider {...settings}>
+            <div className="relative w-full">
+              <img
+                src="/images/12.jpg"
+                alt=""
+                className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+              />
+            </div>
+            <div className="relative w-full">
+              <img
+                src="/images/12.jpg"
+                alt=""
+                className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+              />
+            </div>
+            <div className="relative w-full">
+              <img
+                src="/images/12.jpg"
+                alt=""
+                className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+              />
+            </div> 
+          </Slider>
+          <div className="group relative mt-8">
+              <h3 className="text-lg font-semibold leading-6 text-white">
+                  Travel
+              </h3>
+              <p className="mt-5 line-clamp-3 text-sm leading-6 text-white">ああ</p>
+            </div>
+          </div>
+
+          <div>
+          <Slider {...settings}>
+            <div className="relative w-full">
+              <img
+                src="/images/12.jpg"
+                alt=""
+                className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+              />
+            </div>
+            <div className="relative w-full">
+              <img
+                src="/images/12.jpg"
+                alt=""
+                className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+              />
+            </div>
+            <div className="relative w-full">
+              <img
+                src="/images/12.jpg"
+                alt=""
+                className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+              />
+            </div> 
+          </Slider>
+          <div className="group relative mt-8">
+              <h3 className="text-lg font-semibold leading-6 text-white">
+                  ああ
+              </h3>
+              <p className="mt-5 line-clamp-3 text-sm leading-6 text-white">ああ</p>
+            </div>
+          </div>
+
+          <div>
+          <Slider {...settings}>
+            <div className="relative w-full">
+              <img
+                src="/images/12.jpg"
+                alt=""
+                className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+              />
+            </div>
+            <div className="relative w-full">
+              <img
+                src="/images/12.jpg"
+                alt=""
+                className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+              />
+            </div>
+            <div className="relative w-full">
+              <img
+                src="/images/12.jpg"
+                alt=""
+                className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+              />
+            </div> 
+          </Slider>
+            <div className="group relative mt-8">
+              <h3 className="text-lg font-semibold leading-6 text-white">
+                  ああ
+              </h3>
+              <p className="mt-5 line-clamp-3 text-sm leading-6 text-white">ああ</p>
+            </div>
+            </div>
         </div>
       </div>
     </div>
